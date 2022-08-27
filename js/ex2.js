4388571f90a9438dea212f5cd687b16a36898bd1
vkgoud62@gmail.com <vkgoud62@gmail.com>
2022-08-27 15:59:54 +0530

    your commit message

---
+++ js/ex2.js
@@ -0,0 +1,38 @@
+/* function celsiusToFahrenheit(Celsius){
+    var fahrenheit = (9 * Celsius / 5) + 32;
+    return fahrenheit;
+}
+var celsius1=parseFloat (document.getElementById("celsius_1").innerHTML);
+document.getElementById("fahr_1").innerHTML =celsiusToFahrenheit(celsius1);
+
+var celsius2=parseFloat (document.getElementById("celsius_2").innerHTML);
+document.getElementById("fahr_2").innerHTML =celsiusToFahrenheit(celsius2);
+
+var celsius3=parseFloat (document.getElementById("celsius_3").innerHTML);
+document.getElementById("fahr_3").innerHTML =celsiusToFahrenheit(celsius3); */
+
+
+
+/* var classification = [ "John Hill" , "Mary Jane", "Gary Vee", "Paricia Mills", "Helen Hall", "Paul Green" ]; 
+                
+document.getElementById("best_students").innerHTML = classification.slice(-3); */
+
+/* var course = {
+    'title': "Learn to Code in Python 3",
+    'categories': ['programming', 'technology', 'python'],
+    '5_stars_reviews': 420,
+    '4_stars_reviews': 80,
+    '3_stars_reviews': 33,
+    '2_stars_reviews': 20,
+    '1_stars_reviews': 4,
+    'total_reviews': function(){
+        var total = this["5_stars_reviews"]+ this["4_stars_reviews"]+this["3_stars_reviews"]+this["2_stars_reviews"]+this["1_stars_reviews"];
+    return total;    }
+}
+  var percentage = (course['5_stars_reviews']/course.total_reviews()*100); 
+         document.getElementById("reviews_5_stars").innerHTML=percentage; */ 
+
+
+
+         var shoppingList = ["Milk", "Butter", "Juice", "Bread", "Beer", "Rice", "Potatoes", "Chocolate"];
+         shoppingList.pop("Chocolate");
---
+++ js/scripts.js
@@ -0,0 +1,183 @@
+/* /* function sum_numbers() {
+    var num1 = 5;
+    var num2 = 2;
+    var sum  = x + y;
+
+    alert(sum);
+ */
+
+
+   
+    /* function sum_args(num1,num2) {
+        var sum  = num1 + num2;
+        return sum; 
+    }
+      document.getElementById("sum_result").innerHTML =    sum_args(15,23);
+     */
+
+     /*  var students = ["jagath","Havesh","cnu"];
+      alert(students)
+
+      
+    var groups = [ 
+        [ "John" , "Mary" ],
+        [ "Peter" , "Joana", "Andrew", "Julio" ],
+        [ "Caroline" , "Helen", "Mark" ]
+    ]; */
+/*     var courses = [ "HTML", "Python", "PHP" ];
+    alert(courses) */
+
+   /*  var Animals = ["dog","cat","lion","Tiger","deer","bear","elephant","ant"];
+    alert(Animals) */
+
+  /*   var courses = [
+        {
+            'title': 'Learn code in Python 3',
+            'reviews': 6802,
+            'students': 130129,
+            'categories': ['programming', 'technology']
+        },
+
+        {
+            'title': 'Learn PHP -  Beginner to Advanced',
+            'reviews': 1204,
+            'students': 30521,
+            'categories': ['web development', 'programming']
+        },
+
+        {
+            'title': 'Learn Microsoft Excel 2020',
+            'reviews': 4209,
+            'students': 18560,
+            'categories': ['productivity', 'business']
+        }
+        
+    ]; */
+
+
+
+   // Lesson 11 Functions
+
+ /*   function employee[empName,empAdd]{
+       
+       var details = (empName,empAdd);
+alert(details);
+   }
+    alert(employee('vam','olk'));
+    
+ */
+//// Data types and Arrays
+
+//******************************\
+
+/* var students =[
+    ["kanne","lol","abc","pop"],
+    [2 ,3,4,5,7,8],
+    ["qwe","ujh","ukhl","iorfu"]];
+
+ alert(students[1][2]); 
+  */
+
+
+  /*  employee = {
+        'name': 'James Taylor',
+        'yearOfBirth': 1948,
+        'ID': 'SBJ0001',
+        'role': 'IT Analyst'
+    }; */
+      
+        /* var school ={
+            name :"variu",
+            dob : 23,
+             15012002 : 'tej',
+             love:'amshi'
+        };
+   alert(school.name); */
+
+  /*  var courses = [
+    {
+        'title': 'Learn code in Python 3',
+        'reviews': 6802,
+        'students': 130129,
+        'categories': ['programming', 'technology']
+    },
+
+    {
+        'title': 'Learn PHP -  Beginner to Advanced',
+        'reviews': 1204,
+        'students': 30521,
+        'categories': ['web development', 'programming']
+    },
+
+    {
+        'title': 'Learn Microsoft Excel 2020',
+        'reviews': 4209,
+        'students': 18560,
+        'categories': ['productivity', 'business']
+    }
+    
+]; */
+
+  /* var vechicls={
+              model :1234,
+              seatscapacity : 'Natural',
+              name : 'tata',
+              typeofoil: 'petrol',
+
+              remodel : function(){
+     
+                 return this.name+" "+ this.typeofoil;
+              }
+
+  };
+ */
+  
+  /// EVENTS**************
+
+  /*  
+  document.getElementById("click-me").onclick = function(){
+      alert("You clicked the button");  
+  }
+  
+  document.getElementById("hover-me").onmouseover = function(){
+    alert("You moved the cursor on over me");  
+}
+ 
+document.getElementById("leave-me").onmouseout = function(){
+    alert("You moved the cursor out of me");  
+} */
+  
+  
+  
+  
+ /*  <button id="click-me">Click here</button>
+  <button id="hover-me">Move the cursor over here</button>
+  <button id="leave-me">Move the cursor out of here</button> */
+
+
+
+  /* document.onkeydown = function(event) {
+    if(event.keyCode ==65){
+    alert("you've pressed 'A' key");
+    }else {
+        alert("You've pressed different key");
+    }
+};
+
+
+
+
+         
+
+
+document.getElementById("Announcements").innerHTML=function(){
+    alert("am ok its uptodate");
+} */
+
+
+console.log('Welcome to My First Page of js');
+
+
+
+
+
